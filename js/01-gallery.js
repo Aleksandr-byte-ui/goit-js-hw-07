75064d965a062e66f112427f6115fbd44f09ddd7
Aleksandr-Novohachov <novohachovA@ukr.net>
2021-10-22 01:01:51 +0300

    render gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,28 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+
+const galleryContainer = document.querySelector('.gallery');
+
+const imgMarkUp = renderGallery(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', imgMarkUp);
+
+function renderGallery(items) {
+  return items
+    .map(item => {
+      return `<div class="gallery__item">
+            <a class="gallery__link" href=" ">
+                <img
+                    class="gallery__image"
+                    src=" "
+                    data-source=" "
+                    alt=" "
+                />
+             </a>
+        </div>`;
+    })
+    .join(' ');
+}
