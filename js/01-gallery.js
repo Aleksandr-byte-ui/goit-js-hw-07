028317036ffecb5c900ad789bb535093c0f5e47a
Aleksandr-Novohachov <novohachovA@ukr.net>
2021-10-23 20:26:46 +0300

    gallery opening

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -10,6 +10,8 @@ const imgMarkUp = renderGallery(galleryItems);
 
 galleryContainer.insertAdjacentHTML('beforeend', imgMarkUp);
 
+galleryContainer.addEventListener('click', onGalleryClick);
+
 function renderGallery(items) {
   return items
     .map(({ preview, original, description }) => {
@@ -25,4 +27,27 @@ function renderGallery(items) {
         </div>`;
     })
     .join(' ');
+}
+
+function onGalleryClick(event) {
+  event.preventDefault();
+  const instance = basicLightbox.create(`
+  <div class="modal">
+  <img src="${event.target.dataset.source}" class="js-modal-img" width="800" height="600">
+  </div>
+`, {
+  onShow: (instance) => {
+    window.addEventListener("keydown", onKeyboardClick);
+    function onKeyboardClick(event) {
+      if (event.code === 'Escape') { 
+          instance.close();
+          window.removeEventListener('keydown', onKeyboardClick);
+        };
+    };
+
+      instance.element().querySelector('.js-modal-img').addEventListener("click", () => {
+         instance.close();
+     });
+    },
+}).show();
 }
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,18 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+const galleryItemsString = galleryItems.map(item => 
+  `<a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>`
+).join('');
+
+galleryEl.insertAdjacentHTML('afterbegin', galleryItemsString);
+
+let gallery = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+    captionPosition: 'botton',
+});
